e5c1fdb8b452ba18de4b0ee84506e224a8c74272
pom <>
2019-06-08 15:55:35 +0200

    Update to npx format

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 
 Starter kit for navody-frontend static site generated from ninja templates
 
+## Usage
+
+```
+npm init navody-frontend my-static-page
+```
+
+or 
+
+```
+npx create-navody-frontend my-static-page
+```
+
+
 
--- init.js
+++ init.js
@@ -1,9 +1,23 @@
 #!/usr/bin/env node
 const fs = require('fs-extra')
+const path = require('path')
 
-console.log('test')
-console.log(process.argv)
+console.log('Creating from template')
 
-//const dest = '';
-//fs.copy('./template/*', dest)
+const projectName = process.argv[2]
+
+if (projectName){
+  const dest = path.join(process.cwd(), projectName);
+  fs.mkdir(dest)
+  fs.copy(path.join(__dirname, './template'), dest)
+
+  console.log(`Created project at "${dest}" 
+  \n don't forget to "npm install"`)
+
+  // + install dependencies
+  // + update package.json
+
+} else {
+  throw('Destination not defined')
+}
 
